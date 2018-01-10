29c06ce8f22d92d7f86300b68ef4da39fd413c49
Steven <sgoodliff@yahoo.com>
2018-01-10 22:30:07 +0000

    made the linter happy

--- hello.go
+++ hello.go
@@ -1,8 +1,7 @@
-// Package hello
-// Provides very basic function
+// Package hello provides very basic funcions
 package hello
 
 // returns a string
 func hello() string {
-  return "Hello Go!"
+	return "Hello Go!"
 }
